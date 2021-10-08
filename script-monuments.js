b5a06b6389ac821bd0a6b6771bc989bb9b32dcc7
Developeraahna <90317626+Developeraahna@users.noreply.github.com>
2021-10-08 22:46:48 +0530

    Update script-monuments.js

--- script-monuments.js
+++ script-monuments.js
@@ -24,29 +24,6 @@ function showSlides(n, no) {
 /* carousel script -- end*/
 
 
-/* navbar script */
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
-
-
-const navLink = document.querySelectorAll(".nav-link");
-
-navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}
-/* navbar script -- end */
-
-
 /* back to top btn script */
 //Get the button
 var mybutton = document.getElementById("myBtn");
